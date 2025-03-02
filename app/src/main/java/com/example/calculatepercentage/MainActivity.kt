6655c5fa9d9bc8686f1d80d17e24223d252e139a
ricar <ricardosr4@gmail.com>
2025-03-01 22:46:34 -0300

    logic home screen ok.

--- app/src/main/java/com/example/calculatepercentage/MainActivity.kt
+++ app/src/main/java/com/example/calculatepercentage/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.calculatepercentage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.example.calculatepercentage.ui.screen.home.HomeScreen
 import com.example.calculatepercentage.ui.theme.CalculatePercentageTheme
 
